e34291e747efc1bf972e72c26d241ab1cf5156cb
Denis Nikitin <dinya.nikitin@gmail.com>
2021-10-17 18:18:47 +0300

    png fix

--- webpack.common.js
+++ webpack.common.js
@@ -43,7 +43,7 @@ module.exports = {
             loader: 'url-loader',
             options: {
               limit: 8192,
-              name: 'assets/[hash]/[ext]',
+              
             },
           },
         ],
